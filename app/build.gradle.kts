a72a54e5b46b4b648d69338936d52d55fa7eb513
GhostgramLabs <207371342+ghostgramlabs@users.noreply.github.com>
2026-04-17 21:58:01 -0500

    chore(version): bump to 1.0.12 (13)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.ghostgramlabs.directserve"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.0.11"
+        versionCode = 13
+        versionName = "1.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
