b82d3da535752fbf1416e5c37e82314e7d99f790
GhostgramLabs <207371342+ghostgramlabs@users.noreply.github.com>
2026-04-27 16:51:54 -0500

    chore(release): bump version code to 23 for Play Store compliance

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.ghostgramlabs.directserve"
         minSdk = 26
         targetSdk = 35
-        versionCode = 20
-        versionName = "1.0.19"
+        versionCode = 23
+        versionName = "1.0.21"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
